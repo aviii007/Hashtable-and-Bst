16f61bf1a4cf2470469b14b74f2567737b8a9a92
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-27 16:23:37 +0530

    Added the put method to map key and value.

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,7 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Hashtable;
+
 public class HashTable {
     public static void main(String[] args) {
+        Hashtable<Integer, String> h = new Hashtable<>();// Created Hashtable
+
+        // Adding the mappings to hashtable
+        h.put(1, "H");
+        h.put(2, "A");
+        h.put(3, "S");
+        h.put(4, "H");
+        h.put(5, "T");
+        h.put(6, "A");
+        h.put(7, "B");
+        h.put(8, "L");
+        h.put(9, "E");
+
         System.out.println("Welcome to the Hash table");
+        System.out.println(h);
+
     }
 }
